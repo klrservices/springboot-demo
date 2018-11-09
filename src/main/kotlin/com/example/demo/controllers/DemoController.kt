2bb6156a6eecf557aa543abba8322defbaf9bb60
luromasz <user@email.com>
2018-11-09 22:28:47 +0100

    Added config map to k8s with application.properties

--- src/main/kotlin/com/example/demo/controllers/DemoController.kt
+++ src/main/kotlin/com/example/demo/controllers/DemoController.kt
@@ -1,10 +1,14 @@
 package com.example.demo.controllers
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DemoController {
+    @Value("\${demo.name}")
+    val name:String? = "World"
+
     @RequestMapping("/test")
-    fun test() = "Hello World!!!!"
+    fun test() = "Hello $name!!!!"
 }
